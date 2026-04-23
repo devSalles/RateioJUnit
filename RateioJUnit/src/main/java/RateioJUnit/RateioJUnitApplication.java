1c08310deb59af377f9cda6217b56fd8f9a3b13e
Bernardo de Salles Cruz <168240480+devSalles@users.noreply.github.com>
2026-04-23 15:26:31 -0300

    Realizando upload do projeto

---
+++ RateioJUnit/src/main/java/RateioJUnit/RateioJUnitApplication.java
@@ -0,0 +1,13 @@
+package RateioJUnit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateioJUnitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RateioJUnitApplication.class, args);
+	}
+
+}
