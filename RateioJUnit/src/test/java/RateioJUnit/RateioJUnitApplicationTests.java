af37d8309f9d514ef81a5ee701a212baa1f61663
Bernardo de Salles Cruz <168240480+devSalles@users.noreply.github.com>
2026-04-23 15:26:31 -0300

    Realizando upload do projeto

---
+++ RateioJUnit/src/test/java/RateioJUnit/RateioJUnitApplicationTests.java
@@ -0,0 +1,13 @@
+package RateioJUnit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateioJUnitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
