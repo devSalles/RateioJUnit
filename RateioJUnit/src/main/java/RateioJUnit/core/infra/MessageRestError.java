da7865fa05e835faa1cad2d172c7df3de3b50d83
Bernardo Salles <bernardo.salles@GENOMA.COM>
2026-04-28 17:56:07 -0300

    Desenvolvido a classe de formatação de mensagem de exceção

--- RateioJUnit/src/main/java/RateioJUnit/core/infra/MessageRestError.java
+++ RateioJUnit/src/main/java/RateioJUnit/core/infra/MessageRestError.java
@@ -1,4 +1,39 @@
 package RateioJUnit.core.infra;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class MessageRestError {
+
+    private LocalDateTime timeStamp;
+    private String message;
+    private HttpStatus status;
+
+    private Map<String,String>errorsMessage = new HashMap<>();
+
+
+    public MessageRestError(HttpStatus status, String message) {
+        this.status = status;
+        this.message = message;
+        this.timeStamp = LocalDateTime.now();
+    }
+
+    public MessageRestError(HttpStatus status, String message, Map<String,String> errorsMessage)
+    {
+        this.status = status;
+        this.timeStamp = LocalDateTime.now();
+        this.message = message;
+        this.errorsMessage = errorsMessage;
+    }
 }
