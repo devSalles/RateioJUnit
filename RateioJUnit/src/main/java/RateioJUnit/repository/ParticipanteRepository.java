bbc70cbd3cba192f37772c642d6d38301ff62215
Bernardo Salles <bernardo.salles@GENOMA.COM>
2026-04-29 18:00:26 -0300

    Realizado a implementação dos repositórios

--- RateioJUnit/src/main/java/RateioJUnit/repository/ParticipanteRepository.java
+++ RateioJUnit/src/main/java/RateioJUnit/repository/ParticipanteRepository.java
@@ -1,4 +1,11 @@
 package RateioJUnit.repository;
 
-public interface ParticipanteRepository {
+import RateioJUnit.entity.Participante;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ParticipanteRepository extends JpaRepository<Participante, Long> {
+
+    boolean existsByEmail(String email);
 }
--- RateioJUnit/src/main/java/RateioJUnit/repository/SaldoRepository.java
+++ RateioJUnit/src/main/java/RateioJUnit/repository/SaldoRepository.java
@@ -1,4 +1,9 @@
 package RateioJUnit.repository;
 
-public interface SaldoRepository {
+import RateioJUnit.entity.Despesa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SaldoRepository  extends JpaRepository<Despesa, Long> {
 }
