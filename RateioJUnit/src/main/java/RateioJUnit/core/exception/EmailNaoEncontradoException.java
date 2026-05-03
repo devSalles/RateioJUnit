de5f0244a5890cce15557c03bef1fe734c5a1db1
Bernardo de Salles Cruz <sallesbernardo89@gmail.com>
2026-05-03 19:48:26 -0300

    Criada exceçõe para nome e email não encontrado, email repetido, id não encontrado, e exceção caso participante possua dispesas em seu histórico + realizado o tratamento das mesmas

--- RateioJUnit/src/main/java/RateioJUnit/core/exception/EmailNaoEncontradoException.java
+++ RateioJUnit/src/main/java/RateioJUnit/core/exception/EmailNaoEncontradoException.java
@@ -4,4 +4,7 @@ public class EmailNaoEncontradoException extends RuntimeException {
     public EmailNaoEncontradoException(String message) {
         super(message);
     }
+    public EmailNaoEncontradoException() {
+        super("Email de participante não encontrado");
+    }
 }
