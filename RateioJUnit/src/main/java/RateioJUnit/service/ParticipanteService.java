b181272c9c341d1ab9a7343f4e784eb9452cae16
Bernardo de Salles Cruz <sallesbernardo89@gmail.com>
2026-05-03 19:56:04 -0300

    Implementado o CRUD completo no service de participante + implementação da controller

--- RateioJUnit/src/main/java/RateioJUnit/service/ParticipanteService.java
+++ RateioJUnit/src/main/java/RateioJUnit/service/ParticipanteService.java
@@ -1,8 +1,6 @@
 package RateioJUnit.service;
 
-import RateioJUnit.core.exception.EmailRepetidoCadastradoException;
-import RateioJUnit.core.exception.IdNaoEncontradoException;
-import RateioJUnit.core.exception.NenhumRegistroException;
+import RateioJUnit.core.exception.*;
 import RateioJUnit.dto.usuario.ParticipanteResponseDTO;
 import RateioJUnit.dto.usuario.ParticipanteResquestDTO;
 import RateioJUnit.entity.Participante;
@@ -34,12 +32,12 @@ public class ParticipanteService {
 
     public ParticipanteResponseDTO atualizarParticipante(Long idParticipante,ParticipanteResquestDTO participanteDTO)
     {
-        if(this.participanteRepository.existsByEmail(participanteDTO.email()))
+        Participante participante = buscarID(idParticipante);
+
+        if(!participante.getEmail().equals(participanteDTO.email()) && this.participanteRepository.existsByEmail(participanteDTO.email()))
         {
             throw new EmailRepetidoCadastradoException();
         }
-
-        Participante participante = buscarID(idParticipante);
 
         participanteDTO.updateParticipante(participante);
         this.participanteRepository.save(participante);
@@ -49,8 +47,8 @@ public class ParticipanteService {
 
     public ParticipanteResponseDTO buscarIDParticipante(Long idParticipante)
     {
-         Participante paeticipanteID = buscarID(idParticipante);
-         return ParticipanteResponseDTO.fromParticipante(paeticipanteID);
+         Participante participanteID = buscarID(idParticipante);
+         return ParticipanteResponseDTO.fromParticipante(participanteID);
     }
 
     public List<ParticipanteResponseDTO> buscarTodosParticipantes()
@@ -64,12 +62,53 @@ public class ParticipanteService {
 
         return participantes.stream().map(ParticipanteResponseDTO::fromParticipante).toList();
     }
-    
+
+    public ParticipanteResponseDTO buscarPorEmail(String email)
+    {
+        Participante participanteEmail = this.participanteRepository.findByEmail(email).orElseThrow(EmailNaoEncontradoException::new);
+        return ParticipanteResponseDTO.fromParticipante(participanteEmail);
+    }
+
+    public List<ParticipanteResponseDTO> buscarPorNome(String nomeParticipante)
+    {
+        List<Participante> participanteNome = this.participanteRepository.findByNome(nomeParticipante);
+
+        if(participanteNome.isEmpty())
+        {
+            throw new NomeNaoEncontradoException();
+        }
+
+        return participanteNome.stream().map(ParticipanteResponseDTO::fromParticipante).toList();
+    }
+
+    public ParticipanteResponseDTO deletarParticipante(Long idParticipante)
+    {
+        Participante participante = buscarID(idParticipante);
+
+        validacaoExclusao(idParticipante);
+
+        this.participanteRepository.delete(participante);
+        return ParticipanteResponseDTO.fromParticipante(participante);
+    }
 
     //------------- Metodos Auxiliares -------------
 
     public Participante buscarID(Long idParticipante){
         return this.participanteRepository.findById(idParticipante)
-                .orElseThrow(()->new IdNaoEncontradoException("Id de participante não encontrado"));
+                .orElseThrow(()->new IdNaoEncontradoException("Participante não encontrado"));
+    }
+
+    public void validacaoExclusao(Long idParticipante)
+    {
+        //Verifica se participante possui despesa pagas
+        boolean possuiDespesasPagas = this.participanteRepository.existsBydespesasPagas(idParticipante);
+
+        //Verifica se participante possui despesas no histórico
+        boolean possuiVinculoComoParticipante = this.participanteRepository.existsByDespesasPagasPagador(idParticipante);
+
+        if(possuiDespesasPagas||possuiVinculoComoParticipante)
+        {
+            throw new ParticipantePossuiDespesasException();
+        }
     }
 }
