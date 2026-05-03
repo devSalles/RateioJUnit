c11bedcda8e032aab57c434f80d715b7038193b6
Bernardo de Salles Cruz <sallesbernardo89@gmail.com>
2026-05-03 19:50:58 -0300

    Foi criado os metodos de buscar por nome e email e metodos que realizaram a verificação de despesas (participante com vínculo ou despesas pagas)

--- RateioJUnit/src/main/java/RateioJUnit/repository/ParticipanteRepository.java
+++ RateioJUnit/src/main/java/RateioJUnit/repository/ParticipanteRepository.java
@@ -4,8 +4,21 @@ import RateioJUnit.entity.Participante;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface ParticipanteRepository extends JpaRepository<Participante, Long> {
 
+    List<Participante> findByNome(String nomeParticipante);
+
+    Optional<Participante> findByEmail(String emailParticipante);
+
     boolean existsByEmail(String email);
+
+    //Verifica se participante possui despesa pagas
+    boolean existsBydespesasPagas(Long idParticipante);
+
+    //Verifica se participante possui despesas no histórico
+    boolean existsByDespesasPagasPagador(Long idParticipante);
 }
