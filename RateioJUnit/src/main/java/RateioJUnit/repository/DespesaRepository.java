366d99b00879fbdbdf8af3ba33680c1d8918625a
Bernardo Salles <bernardo.salles@GENOMA.COM>
2026-05-21 10:55:14 -0300

    Adicionado metodos de pesquisa no repositório de despesa para pesquisar por status + retirada de metodo de update na DTO de despesa

--- RateioJUnit/src/main/java/RateioJUnit/repository/DespesaRepository.java
+++ RateioJUnit/src/main/java/RateioJUnit/repository/DespesaRepository.java
@@ -1,9 +1,17 @@
 package RateioJUnit.repository;
 
+import RateioJUnit.enums.StatusDespesa;
+import RateioJUnit.enums.TipoDivisao;
 import RateioJUnit.entity.Despesa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface DespesaRepository  extends JpaRepository<Despesa, Long> {
+
+    List<Despesa> findByStatusDespesa(StatusDespesa statusDespesa);
+
+    List<Despesa> findByTipoDivisao(TipoDivisao tipoDivisao);
 }
