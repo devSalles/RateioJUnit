8dc43215591f954115ce84629f0eebe727019ee9
Bernardo de Salles Cruz <sallesbernardo89@gmail.com>
2026-06-03 17:56:12 -0300

    Desenvovlido metodo de pesquisa no repository e service de Saldo

--- RateioJUnit/src/main/java/RateioJUnit/repository/SaldoRepository.java
+++ RateioJUnit/src/main/java/RateioJUnit/repository/SaldoRepository.java
@@ -5,8 +5,16 @@ import RateioJUnit.entity.Saldo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SaldoRepository  extends JpaRepository<Saldo, Long> {
 
+    List<Saldo> findByCredor(Long idCredor);
 
+    List<Saldo> findByDevedor(Long idDevedor);
+
+    List<Saldo> findByCredorIdOrDevedorId(Long idCredor, Long idDevedor);
+
+    List<Saldo> findByCredorIdAndDevedorId(Long idDevedor, Long idCredor);
 }
