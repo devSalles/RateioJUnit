e4b5adb877a1f423af2edf30ba5c9cfbbb34e5ff
Bernardo de Salles Cruz <sallesbernardo89@gmail.com>
2026-06-14 20:28:34 -0300

    Criado metodos para a criação de entidades de despesa, participante e saldo

--- RateioJUnit/src/test/java/RateioJUnit/factory/SaldoFactory.java
+++ RateioJUnit/src/test/java/RateioJUnit/factory/SaldoFactory.java
@@ -1,4 +1,29 @@
 package RateioJUnit.factory;
 
+import RateioJUnit.entity.Despesa;
+import RateioJUnit.entity.Participante;
+import RateioJUnit.entity.Saldo;
+import RateioJUnit.enums.StatusDespesa;
+import RateioJUnit.enums.TipoDivisao;
+
+import java.math.BigDecimal;
+
 public class SaldoFactory {
+
+    public static Saldo criarSaldo(String nome)
+    {
+        Participante credor = ParticipanteFactory.criarParticipantePersonalizado(1L,"Thais");
+        Participante devedor = ParticipanteFactory.criarParticipantePersonalizado(2L,"Pedro");
+
+        Despesa despesa = DespesaFactory.criarDespesa(1L,new BigDecimal("300.00"), StatusDespesa.CRIADA, TipoDivisao.IGUAL);
+
+        Saldo saldo = new Saldo();
+        saldo.setId(1L);
+        saldo.setValor(new BigDecimal("300.00"));
+        saldo.setDespesa(despesa);
+        saldo.setDevedor(devedor);
+        saldo.setCredor(credor);
+
+        return saldo;
+    }
 }
