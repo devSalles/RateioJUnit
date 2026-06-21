1063d1149e0a469983a0b7ca57a755ee3efeb6e3
Bernardo de Salles Cruz <sallesbernardo89@gmail.com>
2026-06-20 22:31:30 -0300

    Adicionado metodos de teste para atualização de cadastro de participante e lançamento de exceção caso email já existir

--- RateioJUnit/src/test/java/RateioJUnit/service/ParticipanteServiceTest.java
+++ RateioJUnit/src/test/java/RateioJUnit/service/ParticipanteServiceTest.java
@@ -1,9 +1,12 @@
 package RateioJUnit.service;
 
 
+import RateioJUnit.core.exception.IdNaoEncontradoException;
 import RateioJUnit.core.exception.participante.EmailRepetidoCadastradoException;
+import RateioJUnit.dto.usuario.ParticipanteResponseDTO;
 import RateioJUnit.dto.usuario.ParticipanteResquestDTO;
 import RateioJUnit.entity.Participante;
+import RateioJUnit.factory.ParticipanteFactory;
 import RateioJUnit.repository.ParticipanteRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,7 +14,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -25,9 +30,10 @@ public class ParticipanteServiceTest {
     @InjectMocks
     private ParticipanteService participanteService;
 
+    // ---METODOS DE SALVAR PARTICIPANTE ---
 
     @Test
-    void  salvarParticipante()
+    void deveSalvarParticipante()
     {
         ParticipanteResquestDTO participanteResquestDTO = new ParticipanteResquestDTO("Bernardo","sallesbernardo89@gmail.com");
 
@@ -37,7 +43,7 @@ public class ParticipanteServiceTest {
     }
 
     @Test
-    void deveLancarExcecaoQuandoEmailRepetido()
+    void deveLancarExcecaoQuandoEmailJaExistir()
     {
 
         ParticipanteResquestDTO participanteResquestDTO = new ParticipanteResquestDTO("Bernardo","bernardo@gmail.com");
@@ -47,4 +53,43 @@ public class ParticipanteServiceTest {
         assertThrows(EmailRepetidoCadastradoException.class,()->participanteService.salvarParticipante(participanteResquestDTO));
         verify(participanteRepository,never()).save(any(Participante.class));
     }
+
+    // ---METODOS DE ATUALIZAR PARTICIPANTE ---
+
+    @Test
+    void deveAtualizarParticipante()
+    {
+        Long idParticipante = 1L;
+
+        Participante participante = ParticipanteFactory.criarParticipante();
+        ParticipanteResquestDTO resquestDTO = new ParticipanteResquestDTO("bernardo","bernardo@gmail.com");
+
+        when(participanteRepository.findById(idParticipante)).thenReturn(Optional.of(participante));
+        when(participanteRepository.existsByEmailAndIdNot("bernardo@gmail.com",idParticipante)).thenReturn(false);
+
+        ParticipanteResponseDTO response = participanteService.atualizarParticipante(idParticipante,resquestDTO);
+        assertNotNull(response);
+
+        assertEquals("bernardo",response.nome());
+        assertEquals("bernardo@gmail.com",response.email());
+
+        verify(participanteRepository).save(participante);
+    }
+
+    @Test
+    void deveLancarExcecaoQuandoEmailJaEstiverCadastrado()
+    {
+        Long idParticipante = 1L;
+
+        Participante participante = ParticipanteFactory.criarParticipante();
+        ParticipanteResquestDTO requestDTO = new ParticipanteResquestDTO("pedrin","bernardo@gmail.com");
+
+        when(participanteRepository.findById(idParticipante)).thenReturn(Optional.of(participante));
+        when(participanteRepository.existsByEmailAndIdNot("bernardo@gmail.com",idParticipante)).thenReturn(true);
+
+        assertThrows(EmailRepetidoCadastradoException.class,()->participanteService.atualizarParticipante(idParticipante,requestDTO));
+
+        verify(participanteRepository,never()).save(any(Participante.class));
+    }
+    
 }
